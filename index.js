cc029f79d421ad3a7b53a9576c1c1c0a645685d7
Smart Guy Codes <smartguycodes@gmail.com>
2021-10-06 13:59:43 +0300

    Strings: SKip the first character

--- index.js
+++ index.js
@@ -42,6 +42,44 @@ function getLastCharacter(name){
 console.log(getLastCharacter("Ricline"));
 console.log(getLastCharacter("Amelia"));
 
+function skipFirstCharacter(text){
+	return text.substring(1);
+}
+console.log(skipFirstCharacter("Xcode"));
+console.log(skipFirstCharacter("Hello"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

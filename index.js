70c7e81ef334bd58e86d577154c8b8b28399e01b
Smart Guy Codes <smartguycodes@gmail.com>
2021-10-06 13:42:52 +0300

    JS Strings upper and lower cases covered

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ console.log(multiply(2, 4));
 console.log(multiply(3, 2));
 
 //2. STRINGS
+function getCharStrength(str){
+	return str.length;
+}
+console.log(getCharStrength('Smart'));
+console.log(getCharStrength('Smart Guy Who Codes!'));
 
+function shoutMyName(name){
+	return name.toUpperCase();
+}
+console.log(shoutMyName('charles'));
+console.log(shoutMyName('smart guy who codes'));
+
+function lowerName(name){
+	return name.toLowerCase();
+}
+console.log(lowerName('Charles'));
+console.log(lowerName('Smart Guy WHo Codes'));

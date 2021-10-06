cab040a97afa3d7d26ebcd80793dba95a7aa2923
Smart Guy Codes <smartguycodes@gmail.com>
2021-10-06 13:17:11 +0300

    JS Basics covered

---
+++ index.js
@@ -0,0 +1,15 @@
+//1. BASIC FUNCTIONS
+function sum(a, b){
+	return a + b;
+}
+console.log(sum(1, 4));
+console.log(sum(150, 350));
+
+function multiply(a, b){
+	return a * b;
+}
+console.log(multiply(2, 4));
+console.log(multiply(3, 2));
+
+//2. STRINGS
+
